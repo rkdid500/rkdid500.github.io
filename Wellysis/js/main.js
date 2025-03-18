4d7f5c6574196413e5a4281ea9058bce08640af2
rkdid500 <rkdid500@naver.com>
2025-03-19 05:58:03 +0900

    first commit

---
+++ Wellysis/js/main.js
@@ -0,0 +1,33 @@
+// jQuery
+$(function(){
+    // $(".nav").css("display","none");
+    // let bool = true;
+    // $(".nav_btn").click(function(){
+    //     if(bool){
+    //         $(".nav").css("display","block");
+    //     }
+    //     else{
+    //         $(".nav").css("display","none");
+    //     }
+    //     bool = !bool;
+    // });
+    $(".nav").css("display","none");
+    $(".nav_btn").click(function(){
+        $(".nav").toggle();
+    })
+});
+
+// Swiper
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoHeight : true,
+    centeredSlides: false,
+    loop:true,
+    autoplay: {
+      delay : 6000,
+      disableOnInteraction: false,
+  },
+  speed:500,
+  });
